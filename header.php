266be32c9614e2646551394095c949a898e68cfa
David Van Beveren <david.vanbeveren@gmail.com>
2019-02-16 22:12:33 +0100

    Edited header.php with a 2 in the title

--- header.php
+++ header.php
@@ -7,7 +7,7 @@ include_once __DIR__.'/configs/config.php';
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
-	<title>My Vegan Tree | Track your Vegan Impact!</title>
+	<title>My Vegan Tree 2 | Track your Vegan Impact!</title>
 
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
