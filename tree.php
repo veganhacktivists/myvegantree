76144acc74dea6891fe34742752e264d2fb2e3bc
David Van Beveren <david.vanbeveren@gmail.com>
2019-02-20 04:18:43 +0100

    edit link now goes to customize.php

--- tree.php
+++ tree.php
@@ -110,7 +110,7 @@ function get_child($cid){
 
 <div id="color-key" style="position: absolute;">
 <div class="input-color" style="background-color: #ffffff;padding: 10px;margin: 15px;width: 230px;border: 1px solid #EEE;border-radius: 10px;">
-<a href="" style="float:right;color: #da6161;"><i>edit</i></a>
+<a href="customize" style="float:right;color: #da6161;"><i>edit</i></a>
 <table style="margin: 5px;">
   <tr>
     <td style="padding-top: 2px;"><div class="color-box" style="margin-bottom: 4px;padding-left: 5px;padding-top: 2px;background-color: #06bf01;color:#fff;"><i class="fas fa-leaf fa-fw"></i></div></td>
