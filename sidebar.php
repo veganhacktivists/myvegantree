554e14d209f4a4ab169b53ac5b13df1ccb9cd564
David Van Beveren <david.vanbeveren@gmail.com>
2019-03-01 17:51:24 +0100

    Contact, Credits, Donate - added to sidebar

--- sidebar.php
+++ sidebar.php
@@ -65,5 +65,6 @@
     </center>
 
   </div>
-
+<center><a href="https://veganhacktivists.org" style="color: #fff;" target="_blank">Credits</a> <b style="color:#fff;">•</b> <a href="https://youaretheirvoice.com/contact?myvegantree" style="color: #fff;" target="_blank">Contact</a> <b style="color:#fff;">•</b> <a href="https://www.patreon.com/youaretheirvoice" style="color: #fff;" target="_blank">Donate</a></center>
 </div>
+					<script type="text/javascript" src="//downloads.mailchimp.com/js/signup-forms/popup/unique-methods/embed.js" data-dojo-config="usePlainJson: true, isDebug: false"></script><script type="text/javascript">window.dojoRequire(["mojo/signup-forms/Loader"], function(L) { L.start({"baseUrl":"mc.us20.list-manage.com","uuid":"0baba35be8f6397f7ac1066f1","lid":"5fd11d4221","uniqueMethods":true}) })</script>
